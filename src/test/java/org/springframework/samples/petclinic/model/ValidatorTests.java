258f2d654b64c19707158cf66fe0e1cb0aeab31d
Viet Vu <llv43@txstate.edu>
2024-02-14 17:53:05 -0600

    test commit

--- src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
+++ src/test/java/org/springframework/samples/petclinic/model/ValidatorTests.java
@@ -32,6 +32,7 @@ import jakarta.validation.Validator;
  * @author Michael Isvy Simple test to make sure that Bean Validation is working (useful
  * when upgrading to a new version of Hibernate Validator/ Bean Validation)
  */
+// Viet is here
 class ValidatorTests {
 
 	private Validator createValidator() {
